2c0b4c8324deb81662500d32b8ddefa65c964aff
Raveen K Sharma <raveen08@gmail.com>
2020-09-07 22:07:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,7 +25,15 @@ class Student
   end
   
   
-  
+  def save
+    sql = <<-SQL
+      INSERT INTO songs (name, album) 
+      VALUES (?, ?)
+    SQL
+ 
+    DB[:conn].execute(sql, self.name, self.album)
+ 
+  end
   
   
 end
