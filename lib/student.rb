b9035e66480a9c2104eb8dcdd1fd5602d08a1894
Raveen K Sharma <raveen08@gmail.com>
2020-09-07 22:27:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,8 +7,7 @@ class Student
   attr_accessor :name, :grade 
   attr_reader :id
  
-  def initialize(name, grade, id=nil)
-    @id = id
+  def initialize(name, grade)
     @name = name
     @grade = grade
   end
